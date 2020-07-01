0a6d31a09194fc690ff3c3ffed35e57eb7439295
Ottie619 <67606176+Ottie619@users.noreply.github.com>
2020-06-30 23:31:55 -0400

    Add files via upload

---
+++ Worked 2 degree project.py
@@ -0,0 +1,13 @@
+Python 3.8.3 (tags/v3.8.3:6f8c832, May 13 2020, 22:37:02) [MSC v.1924 64 bit (AMD64)] on win32
+Type "help", "copyright", "credits" or "license()" for more information.
+>>> 
+======== RESTART: C:\Users\ottie\Desktop\degrees extract\degrees.py ========
+Loading data...
+Data loaded.
+Name: Emma Watson
+Name: Jennifer Lawrence
+3 degrees of separation.
+1: Emma Watson and Daniel Radcliffe starred in Harry Potter and the Deathly Hallows: Part 1
+2: Daniel Radcliffe and James McAvoy starred in Victor Frankenstein
+3: James McAvoy and Jennifer Lawrence starred in X-Men: First Class
+>>> 
